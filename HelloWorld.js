967f000447b654310c0a28ed5ba82709dcd8dd2b
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-16 14:17:38 +0200

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
+<script>
+info="<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset=&#34;utf-8&#34;>
+    <meta name=&#34;viewport&#34; content=&#34;width=device-width, initial-scale=1&#34;>
     <title>A-Frame React Boilerplate</title>
   </head>
   <body>
-    <div id="sceneContainer" style="height: 100%; width: 100%"></div>
+    <div id=&#34;sceneContainer&#34; style=&#34;height: 100%; width: 100%&#34;></div>
   </body>
-</html>
+</html>"
+
+document.write(info)
+</script>
