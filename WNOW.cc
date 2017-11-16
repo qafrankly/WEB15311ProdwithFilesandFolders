e443135804be6a5d6b106fb67cd2b8157a7c8373
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-16 14:41:33 +0200

    Add files via upload

---
+++ WNOW.cc
@@ -0,0 +1,97 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<tt xmlns="http://www.w3.org/ns/ttml" xmlns:tts="http://www.w3.org/ns/ttml#styling" xmlns:ttm="http://www.w3.org/ns/ttml#metadata" xmlns:wn="http://www.worldnow.com/mvaxml">
+  <head>
+    <styling>
+      <style xml:id="captionsStyle" tts:fontFamily="Arial" tts:color="#ffffffff" tts:backgroundColor="#00000087" />
+    </styling>
+    <layout>
+      <region xml:id="captionsRegion">
+        <style style="captionsStyle" tts:origin="10% 56%" tts:extent="80% 10%" tts:fontSize="100%" tts:textAlign="center" tts:displayAlign="center" />
+      </region>
+    </layout>
+  </head>
+  <body>
+    <div region="captionsRegion" ttm:role="captions">
+      <p begin="00:00:00.617" end="00:00:00.817">TO BE</p>
+      <p begin="00:00:00.850" end="00:00:04.986">DANCING TO ONE OF TYRESE'S NEW SONGS.</p>
+      <p begin="00:00:05.021" end="00:00:06.922">DR. OZ: I'M HEARING THE OTHER</p>
+      <p begin="00:00:06.956" end="00:00:07.789">SIDE AS WELL AND I LOVE THE</p>
+      <p begin="00:00:07.824" end="00:00:08.891">FACT, TYRESE, THAT YOU'RE</p>
+      <p begin="00:00:08.925" end="00:00:10.759">BREAKING THE CYCLE, BECAUSE</p>
+      <p begin="00:00:10.794" end="00:00:13.763">IT'S GOT TO START SOMEWHERE.</p>
+      <p begin="00:00:13.797" end="00:00:15.765">&gt;&gt; HE PUT SWAG IN MY LIFE.</p>
+      <p begin="00:00:15.799" end="00:00:18.768">I HAD AN OFFER FOR MY GROUP,</p>
+      <p begin="00:00:18.802" end="00:00:21.271">RUN D.M.C., TO GET BACK TOGETHER.</p>
+      <p begin="00:00:21.304" end="00:00:22.071">I WAS STARTING TO FIGURE OUT</p>
+      <p begin="00:00:22.105" end="00:00:22.939">HOW IT WAS GOING TO WORK.</p>
+      <p begin="00:00:22.972" end="00:00:25.240">YOU GOT TO PUT THE GROUP BACK TOGETHER.</p>
+      <p begin="00:00:25.275" end="00:00:28.045">WE LOVE "TRICKY".</p>
+      <p begin="00:00:28.078" end="00:00:29.011">SO HE PUSHED ME OVER, GOT</p>
+      <p begin="00:00:29.045" end="00:00:29.945">ENERGETIC AGAIN.</p>
+      <p begin="00:00:29.979" end="00:00:30.947">&gt;&gt; THAT'S 12 YEARS.</p>
+      <p begin="00:00:30.980" end="00:00:32.113">&gt;&gt; WE HADN'T BEEN ON THE ROAD</p>
+      <p begin="00:00:32.148" end="00:00:32.981">IN 12 YEARS.</p>
+      <p begin="00:00:33.016" end="00:00:34.217">TYRESE CAME AND HE PUSHED ME</p>
+      <p begin="00:00:34.250" end="00:00:37.853">AND PUSHED ME, GOT ME MOVING</p>
+      <p begin="00:00:37.887" end="00:00:39.120">AND GOT ME -- I LOST 20 POUNDS</p>
+      <p begin="00:00:39.155" end="00:00:40.890">SINCE I MET THE GUY.</p>
+      <p begin="00:00:40.924" end="00:00:45.162">DR. OZ: YOU LOST 20 POUNDS? [APPLAUSE]</p>
+      <p begin="00:00:45.195" end="00:00:45.896">&gt;&gt; RIGHT.</p>
+      <p begin="00:00:45.929" end="00:00:50.929">SO HIM PUSHING ME, ARE YOU AT</p>
+      <p begin="00:00:51.901" end="00:00:52.834">THE GYM?</p>
+      <p begin="00:00:52.869" end="00:00:53.936">DID YOU EAT YOUR EGG WHITES?</p>
+      <p begin="00:00:53.970" end="00:00:55.805">ARE YOU GOING TO BE ONSTAGE DR</p>
+      <p begin="00:00:55.839" end="00:00:58.808">I PLAY IN THE BORGATA.</p>
+      <p begin="00:00:58.842" end="00:01:01.245">MAKE SURE YOU GOT IT ALL TOGETHER.</p>
+      <p begin="00:01:01.277" end="00:01:02.845">HE'S BEEN REALLY GOOD TO BRING</p>
+      <p begin="00:01:02.879" end="00:01:04.947">THE ENERGY TO ME AND I'VE BEEN</p>
+      <p begin="00:01:04.981" end="00:01:06.114">GOOD ABOUT HOW TO TEACH HIM TO</p>
+      <p begin="00:01:06.149" end="00:01:07.950">BECOME THE HUSBAND WHO ENDS UP</p>
+      <p begin="00:01:07.984" end="00:01:08.416">PUTTING ON THE POUNDS.</p>
+      <p begin="00:01:08.451" end="00:01:11.254">I GOT TO GET BACK TO THE SWAG</p>
+      <p begin="00:01:11.287" end="00:01:13.922">SO I CAN GET DOWN WITH THE</p>
+      <p begin="00:01:13.957" end="00:01:14.925">MUSCULAR, LIKE HIM, AND AS HE'S</p>
+      <p begin="00:01:14.958" end="00:01:17.294">GROWING UP, WE'RE WORKING IT OUT.</p>
+      <p begin="00:01:17.327" end="00:01:18.862">&gt;&gt; FAIR EXCHANGE.</p>
+      <p begin="00:01:18.895" end="00:01:19.895">NO ROBBERY.</p>
+      <p begin="00:01:19.929" end="00:01:21.097">DR. OZ: ARE YOU GETTING HIS</p>
+      <p begin="00:01:21.131" end="00:01:22.899">WARK BOARD ABS?</p>
+      <p begin="00:01:22.932" end="00:01:25.166">&gt;&gt; NO, THEY'RE NOT QUITE THERE YET.</p>
+      <p begin="00:01:25.201" end="00:01:29.137">I WOULDN'T TAKE MY SHIRT OFF HERE.</p>
+      <p begin="00:01:29.172" end="00:01:31.008">IF YOU DO SIT-UPS, I'M JUST</p>
+      <p begin="00:01:31.040" end="00:01:32.874">GOING TO WATCH YOU GUYS.</p>
+      <p begin="00:01:32.909" end="00:01:34.176">DR. OZ: I'M GOING TO TRADE ADVICE.</p>
+      <p begin="00:01:34.210" end="00:01:35.878">I'M GOING TO SHOW YOU WHAT I</p>
+      <p begin="00:01:35.912" end="00:01:37.848">NORMALLY DO AND TYRESE CAN SHOW</p>
+      <p begin="00:01:37.881" end="00:01:39.149">ME WHAT HE NORMALLY DOES.</p>
+      <p begin="00:01:39.182" end="00:01:39.982">&gt;&gt; THAT'S FAIR.</p>
+      <p begin="00:01:40.016" end="00:01:41.249">DR. OZ: FIRST THING YOU DO IS</p>
+      <p begin="00:01:41.284" end="00:01:43.184">YOU GO UP IN A CHAIR LIKE THIS.</p>
+      <p begin="00:01:43.219" end="00:01:44.186">YOU GUYS IN THE AUDIENCE, YOU</p>
+      <p begin="00:01:44.220" end="00:01:45.954">CAN DO THIS, TOO.</p>
+      <p begin="00:01:45.989" end="00:01:47.957">LIFT YOURSELF UP.</p>
+      <p begin="00:01:47.991" end="00:01:51.160">NOW YOU LIFT YOUR LEGS UP LIKE THAT.</p>
+      <p begin="00:01:51.194" end="00:01:51.994">VERY GOOD.</p>
+      <p begin="00:01:52.028" end="00:01:53.928">OH, COME ON NOW.</p>
+      <p begin="00:01:53.963" end="00:01:54.964">&gt;&gt; I'M IN.</p>
+      <p begin="00:01:54.998" end="00:01:57.967">&gt;&gt; TYRESE, PUT THOSE ABS UP.</p>
+      <p begin="00:01:58.001" end="00:02:01.038">&gt;&gt; I'D LOVE TO SHOW YOU, SIR.</p>
+      <p begin="00:02:01.070" end="00:02:02.937">DR. OZ: I'M JUST FEELING THE</p>
+      <p begin="00:02:02.972" end="00:02:05.240">ABS, I'M JUST FEELING THEM.</p>
+      <p begin="00:02:05.275" end="00:02:07.077">&gt;&gt; THEY'RE THERE.</p>
+      <p begin="00:02:07.110" end="00:02:08.045">DR. OZ: I WANT THOSE ABS.</p>
+      <p begin="00:02:08.077" end="00:02:10.345">&gt;&gt; I JUST ATE A LITTLE BIT THIS MORNING.</p>
+      <p begin="00:02:10.380" end="00:02:12.149">I'M GOING TO HOLD OFF ON THAT.</p>
+      <p begin="00:02:12.182" end="00:02:14.183">CAMERA, LET'S GET THIS.</p>
+      <p begin="00:02:14.217" end="00:02:17.087">YOU'VE GOT TO GRAB MY ANKLES</p>
+      <p begin="00:02:17.120" end="00:02:18.221">AND BRING MY FEET UP AND I'M</p>
+      <p begin="00:02:18.254" end="00:02:20.121">GOING TO THROW IT STRAIGHT OUT.</p>
+      <p begin="00:02:20.156" end="00:02:25.156">AND TO THE MIDDLE AND TO THE LEFT.</p>
+      <p begin="00:02:26.429" end="00:02:28.296">STRAIGHT DOWN THE MIDDLE.</p>
+      <p begin="00:02:28.331" end="00:02:31.100">&gt;&gt; I'M PRAYING FOR YOU.</p>
+      <p begin="00:02:31.134" end="00:02:32.102">[APPLAUSE]</p>
+      <p begin="00:02:32.135" end="00:02:34.203">DR. OZ: UP NEXT -- IT'S YOUR</p>
+      <p begin="00:02:34.237" end="00:02:35.137">TURN TO ASK.</p>
+    </div>
+  </body>
+</tt>
