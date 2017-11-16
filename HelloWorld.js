d12b9955b4540b432cb6d62104b9791b142737a0
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-16 14:19:15 +0200

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,15 +1,44 @@
 <script>
-info="<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset=&#34;utf-8&#34;>
-    <meta name=&#34;viewport&#34; content=&#34;width=device-width, initial-scale=1&#34;>
-    <title>A-Frame React Boilerplate</title>
-  </head>
-  <body>
-    <div id=&#34;sceneContainer&#34; style=&#34;height: 100%; width: 100%&#34;></div>
-  </body>
-</html>"
 
-document.write(info)
+// HTML to JavaScript converter
+// By John Krutsch (http://asp.xcentrixlc.com/john)
+// Moderator of the JavaScript Kit Help Forum: http://freewarejava.com/cgi-bin/Ultimate.cgi
+
+function scriptIt(val){
+val.value=val.value.replace(/"/gi,"&#34;")
+val.value=val.value.replace(/'/gi,"&#39;")
+valArr=escape(val.value).split("%0D%0A")
+val.value=""
+for (i=0; i<valArr.length; i++){
+val.value+= (i==0) ? "<script>\ninfo=" : ""
+val.value+= "\"" + unescape(valArr[i])
+val.value+= (i!=valArr.length-1) ? "\" + \n" : "\"\n" 
+}
+val.value+="\ndocument.write(info)\n<\/script>"
+}
+
+function ctrlA(el) {
+with(el){
+focus(); select() 
+}
+if(document.all){
+txt=el.createTextRange()
+txt.execCommand("Copy") 
+window.status='Selected and copied to clipboard!'
+}
+else window.status='Press ctrl-c to copy the text to the clipboard'
+setTimeout("window.status=''",3000)
+} 
+
 </script>
+<center>
+<form name="f">
+<input type="button" value="HTML -> JavaScript" onclick="scriptIt(document.f.t)" />
+<input type="reset" value="Reset">
+<input type="button" value="Select All" onclick="ctrlA(document.f.t)" />
+<br />
+<textarea name="t" cols="75"
+rows="20"></textarea>
+<br />
+</form>
+</center>
